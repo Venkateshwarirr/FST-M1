838793ecf9c736800cb1a129944381d804767adc
Venkateshwarirr <116861297+Venkateshwarirr@users.noreply.github.com>
2022-10-31 17:39:29 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+name = input( "what is your name: " )
+age = int( input( "how old are you: " ) )
+year = str( ( 2023 - age ) + 100 )
+print( name + " will 100 years old in the year " + year )
---
+++ Python/Activities/Activity8.py
@@ -0,0 +1,13 @@
+
+numList = [90, 60, 50, 20, 90]
+print("Given list is ", numList)
+
+firstElement = numList[0]
+
+lastElement = numList[-1]
+
+
+if (firstElement == lastElement):
+    print(True)
+else:
+    print(False)
